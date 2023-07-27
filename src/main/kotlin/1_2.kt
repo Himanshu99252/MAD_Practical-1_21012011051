5313b0afab0169b8630c734523081d2a945e1fed
Himanshu99252 <modihimanshu880@gamil.com>
2023-07-27 22:41:48 +0530

    pr_1_2

---
+++ src/main/kotlin/1_2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var num1:Int=10
+    val num2:Double=num1.toDouble()
+    println("Integer value to double:$num2")
+
+    var a:String = "10"
+    val b:String=a.toString()
+    println("String value to integer:$b")
+
+    var c:String="11.12"
+    val d:Double=c.toDouble()
+    println("String value to double:$d")
+
+}
