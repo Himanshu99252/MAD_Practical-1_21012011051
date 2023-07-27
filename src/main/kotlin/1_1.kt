c6f5eb997b1e712742a79e6a52489a7f3939525c
Himanshu99252 <modihimanshu880@gamil.com>
2023-07-27 22:37:55 +0530

    pr_1

---
+++ src/main/kotlin/1_1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var a: Int = 6
+    println("$a :Integer value")
+    var b: Float = 4.5F
+    println("$b :Float value")
+    var c: Char = 'd'
+    println("$c :Character value")
+    var name: String = "Himanshu"
+    println("$name :String value")
+    var e: Double = 96.36
+    println("$e :Double value")
+    var f: Boolean = false
+    println("$f :Boolean value")
+    var g: Long = 893759490
+    println("$g :Long value")
+    var h: Short = 9
+    println("$h :Short value")
+    var i: Byte = 127
+    println("$i :Byte value")
+}
